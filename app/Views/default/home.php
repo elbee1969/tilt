3afcfcf4012ea7c51156ff655b47fbe89dc81f82
BaptisteCG <baptiste.cresteyg@gmail.com>
2017-07-07 16:38:45 +0200

    'baptiste'

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -1,10 +1,177 @@
-<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+<?php $this->layout('layout', ['title' => 'Tilt! Communauté d\'enseignants et d\'apprenants']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xl-4 col-12 hidden-xs-down hidden-sm-down">
+		</div>
+		<div class="col-xl-4 col-md-8 col-xs-12 ">
+			<p class="titre" id="region">Recherche des Tilters par région</p>
+			<div id="francemap" style="width: 100%; height: 600px;"></div>
+		</div>
+		<div class="col-xl-4 col-xs-12 hidden-xs-down hidden-sm-down">
+				</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<p class="titre" id="concept">Tilt, tout un concept</p>
+		</div>
+</div>
+		<div class="row">
+		<div class="col-md-4 col-12">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/icon1.png" alt=""></p>
+<p class="center subtitle_icon">Recherchez les apprenants et enseignants <br>près de chez vous</p>
+		</div>
+		<div class="col-md-4 col-12">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/icon2.png" alt=""></p>
+<p class="center subtitle_icon">Inscrivez-vous pour prendre contact <br>avec les enseignants et apprenants inscrits</p>
+		</div>
+		<div class="col-md-4 col-12">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/icon3.png" alt=""></p>
+<p class="center subtitle_icon">partagez vos compétences & connaissances <br>dans la bonne humeur !</p>
+		</div>
+	</div>
 
-	
+<div class="row ">
+	<div class="col-12">
+		<p class="titre" id="concept">Les catégories proposées</p>
+	</div>
+</div>
+<div class="row categoryspace">
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_008_Basketball.png" alt=""></p>
+<p class="center ">Sports</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_101_Cook.png" alt=""></p>
+<p class="center">Cuisines</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_015_Globe.png" alt=""></p>
+<p class="center">Géographie</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_068_Mouse.png" alt=""></p>
+<p class="center ">Nouvelles technologies</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_102_Languages.png" alt=""></p>
+<p class="center">Langues</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_082_ColorPalette.png" alt=""></p>
+<p class="center">Arts</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_007_AtomicStructure.png" alt=""></p>
+<p class="center ">Chimie</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_037_Graph.png" alt=""></p>
+<p class="center">Economie</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_031_ReadingBook.png" alt=""></p>
+<p class="center">Français</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_045_Calendar.png" alt=""></p>
+<p class="center ">Histoire</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_019_MathSymbols.png" alt=""></p>
+<p class="center">Mathématiques</p>
+</div>
+<div class="col-md-2 col-6 col-sm-4">
+<p class="center iconhomepage"><img class="categoryicon" src="./assets/img/categories/Education_046_Notes.png" alt=""></p>
+<p class="center">Musique</p>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-12">
+		<p class="titre" id="concept">Nos Tilters adorent ...</p>
+	</div>
+</div>
+<div class="row">
+<div class="col-md-6 col-12 col-lg-3 col-xl-3 col-sm-6">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/testi1.jpg" alt=""></p>
+<a href="#"><p class="center subtitle_icon">Franck R.</p></a>
+<p class="center iconhomepage"><img class="separator" src="./assets/img/separator.png" alt=""></p>
+<p class="center"><i>Se site est tou simpleument extra !<br> Je viens de m'inscrire et j'ai déjà un<br> cour de français prévu dans deux jour ! </i></p>
+</div>
+<div class="col-md-6 col-12 col-lg-3 col-xl-3 col-sm-6">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/testi2.jpg" alt=""></p>
+<a href="#"><p class="center subtitle_icon">Albert E.</p></a>
+<p class="center iconhomepage"><img class="separator" src="./assets/img/separator.png" alt=""></p>
+<p class="center"><i>J'ai vu ce site sur e=m6,<br> Je n'en reviens pas que mon voisin Isaac<br> donne des cours de maths à 5km de chez moi ! </i></p>
+
+</div>
+<div class="col-md-6 col-12 col-lg-3 col-xl-3 col-sm-6">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/testi3.jpg" alt=""></p>
+<a href="#"><p class="center subtitle_icon">Dwayne J.</p></a>
+<p class="center iconhomepage"><img class="separator" src="./assets/img/separator.png" alt=""></p>
+<p class="center"><i>Derrière tout mes muscles<br>se cache une volonté de partage !<br> Rejoignez mon cours de sport intensif ! </i></p>
+</div>
+<div class="col-md-6 col-12 col-lg-3 col-xl-3 col-sm-6">
+<p class="center iconhomepage"><img class="imgiconhomepage rounded-circle" src="./assets/img/testi4.jpg" alt=""></p>
+<a href="#"><p class="center subtitle_icon">Madonna Louise C.</p></a>
+<p class="center iconhomepage"><img class="separator" src="./assets/img/separator.png" alt=""></p>
+<p class="center"><i>Superbe plateforme d'entraide et de service<br> entre particuliers ! Je suis également surprise<br> par l'amabilité des tilters ! </i></p>
+</div>
+</div>
+
+
+
+
+
+
+
+
+<div class="row">
+	<div class="col-12">
+		<p class="titre" id="concept">Et vous, que savez-vous faire de vos 10 doigts ?</p>
+		<p class="center">Devenez Tilteur et révélez toutes vos compétences auprès de la communauté !</p>
+		<p class="center"><img id="coeurplateau" src="./assets/img/tilt.png" alt=""></p>
+		<p class="center" id="propcomp"><button type="button" class="btn btn-primary btn-lg">Proposer mes compétences</button></p>
+	</div>
+</div>
+
+
+
+</div>
+
+
 
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('js') ?>
+<link href="./assets/map/jqvmap.css" media="screen" rel="stylesheet" type="text/css" />
+<script src="./assets/map/jquery.vmap.js" type="text/javascript"></script>
+<script src="./assets/map/jquery.vmap.france.js" type="text/javascript"></script>
+<script src="./assets/map/jquery.vmap.colorsFrance.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#francemap').vectorMap({
+			map: 'france_fr',
+		hoverOpacity: 0.5,
+		hoverColor: "#9fa8da",
+		backgroundColor: "#FFFFFF",
+		color: "#3F51B5",
+		borderColor: "#ffffff",
+		selectedColor: "#9fa8da",
+		enableZoom: true,
+		showTooltip: true,
+			onRegionClick: function(element, code, region)
+			{
+window.location.href = '/test-tilt/public/france_map.html?slug='  // + nom de la region créée en BDD & prêt a lusage ;
+			}
+	});
+});
+</script>
+
+
+
+
+
+<?php $this->stop('js') ?>
--- app/Views/layout.php
+++ app/Views/layout.php
@@ -3,21 +3,118 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/font-awesome.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/normalize.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/animate.css') ?> ">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.css') ?>">
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+	<link rel="icon" type="image/png" href="./assets/img/favicon.png" />
+
+
 </head>
 <body>
-	<div class="container">
-		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
-		</header>
+	<header>
+		<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top" style="background-color:#3f51b5">
+		  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <a class="navbar-brand" href="#"><p id="logo_navbar">Tilt</p>
+			<p class="slogan">Teach it learn that !</p></a>
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li>
+						<a class="nav-link" href="http://www.tilt.fr">Concept</a>
+		      </li>
+					<li>
+						<a class="nav-link" href="http://www.tilt.fr">Communauté</a>
+					</li>
+					<li>
+						<a class="nav-link" href="http://www.tilt.fr">Matières</a>
+					</li>
+					<li>
+						<a class="nav-link" href="http://www.tilt.fr">A propos</a>
+					</li>
+					<li>
+						<a class="nav-link" href="http://www.tilt.fr">Contact</a>
+					</li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		        <a class="nav-link" href="#">Inscription / Connexion</a>
+		      <input class="form-control mr-sm-2" type="text" placeholder="Compétences, régions ..."><button class="btn btn-outline-success my-2 my-sm-0 fa fa-search" aria-hidden="true" type="submit"></button></input>
 
-		<section>
+		    </form>
+		  </div>
+		</nav>
+	</header>
+		<section class="backgroundsection">
 			<?= $this->section('main_content') ?>
 		</section>
 
 		<footer>
-		</footer>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-2 hidden-xs-down hidden-sm-down"><!-- Collone vide !--></div>
+		<div class="col-md-3 col-12">
+			<div class="row">
+			<div class="col-md-12">
+				<p id="logo_footer">Tilt</p>
+			</div>
+		</div>
+			<div class="row">
+				<div class="col-md-7 col-6">
+					<ul>
+						<li>Mentions légales</li>
+						<li>Contact</li>
+						<li>CGU</li>
+						<li>FAQ</li>
+				</div>
+				<div class="col-md-5 col-6 ">
+					<li>Top enseignants</li>
+					<li>Top apprenants</li>
+					<li>Top matières</li>
+					<li>Top régions actives</li>
+				</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2 col-12">
+<div class="container-fluid">
+			<div class="row">
+			<div class="col-12 align-self-start">
+				<p class="copyright" id="logo_footer">Rejoignez-nous</p>
+				<p class="copyright">
+					<a target="_blank" href="https://search.itunes.apple.com/WebObjects/MZContentLink.woa/wa/link?mt=8&path=appstore"><i class="fa fa-facebook fa-3x" aria-hidden="true"></i></a>
+					<a target="_blank" href="https://search.itunes.apple.com/WebObjects/MZContentLink.woa/wa/link?mt=8&path=appstore"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+				</p>
+			</div>
+			</div>
+			<div class="row align-items-end">
+			<div class="col-12 align-self-end">
+				<p class="copyright" id="copyright"> © 2017 Tilt | Webforce3 </p>
+			</div>
+			</div>
+		</div>
 	</div>
+		<div class="col-md-2 col-12 align-self-center">
+<p class="copyright"><a target="_blank" href="https://search.itunes.apple.com/WebObjects/MZContentLink.woa/wa/link?mt=8&path=appstore"><img id="appstore" src="./assets/img/download-appstore.png" alt=""></a> <a target="_blank" href="https://play.google.com/store?hl=fr"><img id="googleplay" src="./assets/img/download-googleplay.png" alt=""></a></p>
+		</div>
+		<div class="col-md-2 col-12 align-self-end hidden-xs-down hidden-sm-down">
+<img id="mainiphone" src="https://static1.squarespace.com/static/521d05e2e4b04fdb82ede5d4/t/526c0155e4b039ceb7d8986f/1382809949346/rc_hand-phone-main.png" alt="">
+		</div>
+		<div class="col-md-2 col-12 hidden-xs-down hidden-sm-down"><!-- Collone vide !--></div>
+	</div>
+</div>
+
+		</footer>
+
+	<script src="./assets/js/jquery-3.2.1.js" charset="utf-8"></script>
+	<script type="text/javascript" src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+	<script src="./assets/js/bootstrap.js" charset="utf-8"></script>
+	<?= $this->section('js') ?> <!-- Appel de la section js qui est situé dans home.php - Cela va récupérer les JS de la map  !-->
+
+
+
 </body>
 </html>
