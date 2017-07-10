421c56ce58cf2da378d353ac9effa1771f64bd7a
laurent berthelot <laurent.berthelot1969@gmail.com>
2017-07-10 10:46:22 +0200

    modif userctrl region

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -16,20 +16,20 @@ class DefaultController extends TiltController
 	public function home()
 	{
 
-		// test de la méthode pour trouver les 5 derniers enseignants inscrits
-		$enseignants = new UsersModel();
-		$b = $enseignants->findFiveLastEnseignants();
-		// debug($b);
-
-		// test de la méthode pour trouver les 5 derniers apprenants inscrits
-		$apprenants = new UsersModel();
-		$c = $apprenants->findFiveLastApprenants();
-		// debug($c);
-
-		// test de la méthode pour trouver les utilisareurs en fonction d'une région
-		$users = new UsersModel();
-		$d = $users->findApprenantsInRegion('auvergne');
-		// debug($d);
+		// // test de la méthode pour trouver les 5 derniers enseignants inscrits
+		// $enseignants = new UsersModel();
+		// $b = $enseignants->findFiveLastEnseignants();
+		// // debug($b);
+		//
+		// // test de la méthode pour trouver les 5 derniers apprenants inscrits
+		// $apprenants = new UsersModel();
+		// $c = $apprenants->findFiveLastApprenants();
+		// // debug($c);
+		//
+		// // test de la méthode pour trouver les utilisareurs en fonction d'une région
+		// $users = new UsersModel();
+		// $d = $users->findApprenantsInRegion('auvergne');
+		// // debug($d);
 
 		// test de la méthode pour les icones de la page d'accueil
 		// $icon = new CompetencesModel();
