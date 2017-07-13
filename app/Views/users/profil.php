f5e28b0b081c25201ebb00547c41056a0aa3a9f7
NicolasDec1980 <nicolas.lacour76@gmail.com>
2017-07-13 11:26:07 +0200

    'treizethird'

--- app/Views/users/profil.php
+++ app/Views/users/profil.php
@@ -26,7 +26,7 @@ $this->layout('layout', ['title' => 'profil de '.$w_user['pseudo']]);
           <li class="list-group-item">    <?= $w_user['first_name']; ?>   <?= $w_user['last_name']; ?></li>
           <li class="list-group-item"><?= $w_user['email']; ?></li>
           <li class="list-group-item"><?= 'Inscrit le '.$w_user['created_at']; ?></li>
-          <li class="list-group-item"><?= 'Région : '.$w_user['region_id']; ?></li>
+          <li class="list-group-item"><?= 'Région : '.$regionName['name']; ?></li>
           <?php if(in_array($w_user['role'], ['apprenant', 'enseignant', 'admin'])){ ?>
           <li class="list-group-item"><a href="#">Modifier mes informations</a></li>
           <?php } ?>
