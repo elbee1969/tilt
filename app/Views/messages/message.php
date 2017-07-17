8161ea27dba02a67c49d65b97bdfb91f4b0242b4
BaptisteCG <baptiste.cresteyg@gmail.com>
2017-07-17 14:11:59 +0200

    baptiste17

--- app/Views/messages/message.php
+++ app/Views/messages/message.php
@@ -8,10 +8,6 @@ $this->layout('layout', ['title' => 'Envoyer un message']);
 
 <?php $this->start('main_content'); ?>
 
-<<<<<<< HEAD
-<h1>Page message</h1>
-<?php
-=======
 <div class="container-fluid">
 
   <div class="row">
@@ -26,15 +22,18 @@ $this->layout('layout', ['title' => 'Envoyer un message']);
     <div class="col-12 col-md-8 offset-md-2 offset-0">
 
 
->>>>>>> 5a23ba34f9e7bb4b5efc0fe1053186640575fdb5
-
 <div class="form-group">
   <form id="tchat_form" action="<?= $this->url('messages_message_action', ['id' => $id, 'user_id' => $user_id]); ?>" method="post">
-    <!-- <input style=" display:none;" type="text" name="user_id" value="<?php //if(isset($b)){ echo $b;}; ?>">-->
     <label for="message"></label>
-    <textarea id="message" class="form-controlall" name="message" rows="2" cols="100"><?php if(!empty($article['message'])){ echo $article['message'];}; ?></textarea>
+
+<!-- CODE DE BAPTISTE !-->
+
+    <textarea id="message" class="form-controlall" name="message" placeholder="Votre message ..." rows="2" cols="100"><?php if(!empty($article['message'])){ echo $article['message'];}; ?></textarea>
     <span style="color:red; font-size:0.70em;" ><?php if(!empty($errors['message'])){ echo $errors['message']; }; ?></span>
-    <input class="btn btn-primary btn-lg btn-block" type="submit" name="btnsubmit" value="Envoi message">
+    <input class="btn btn-primary btn-lg btn-block" type="submit" name="btnsubmit" value="Envoyer">
+
+<!-- FIN DU CODE DE BAPTISTE !-->
+
   </form>
 </div>
 <a href="<?= $this->url('messages_messages', ['id' => $id,'user_id' => $user_id]); ?>"><p><i class="fa fa-arrow-left" aria-hidden="true"></i> Retour aux messages</p></a>
